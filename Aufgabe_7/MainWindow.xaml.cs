0f7f1f9b47fe1f72f1352198ab887894a514dddc
Dirk Mueller <dirkmuller8@outlook.de>
2021-03-22 05:56:23 +0100

    Implemented play area (canvas), boarders and timer

--- Aufgabe_7/MainWindow.xaml.cs
+++ Aufgabe_7/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Aufgabe_7
 {
@@ -22,11 +23,66 @@ namespace Aufgabe_7
     {
         int boarderRectangle;
         int remainingTime;
+        int points;
+        DispatcherTimer timerSpielzeit;
         public MainWindow()
         {
             InitializeComponent();
             boarderRectangle = 25;
             remainingTime = 120;
+            points = 0;
+            timerSpielzeit = new DispatcherTimer();
+            //das Intervall setzen
+            timerSpielzeit.Interval = TimeSpan.FromMilliseconds(1000);
+            //die Methode für das Ereignis zuweisen
+            timerSpielzeit.Tick += new EventHandler(Timer_Spielzeit);
+            timerSpielzeit.Start();
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            ZeichneSpielfeld();
+            //timerSpielzeit = new DispatcherTimer();
+            ////das Intervall setzen
+            //timerSpielzeit.Interval = TimeSpan.FromMilliseconds(1000);
+            ////die Methode für das Ereignis zuweisen
+            //timerSpielzeit.Tick += new EventHandler(Timer_Spielzeit);
+            //Duration duration = newDuration(TimeSpan.FromSeconds(20));
+            //DoubleAnimation doubleanimation = newDoubleAnimation(200.0, duration);
+            //PBar.BeginAnimation(ProgressBar.ValueProperty, doubleanimation);
+            zeitAnzeige.Content = remainingTime;
+            punktAnzeige.Content = points;
+        }
+        void ZeichneSpielfeld()
+        {
+            //der Balken oben
+            ZeichneRechteck(new Point(0, 0), spielfeld.ActualWidth, boarderRectangle);
+            //der Balken rechts
+            ZeichneRechteck(new Point(spielfeld.ActualWidth - boarderRectangle, 0), boarderRectangle, spielfeld.ActualHeight);
+            //der Balken unten
+            ZeichneRechteck(new Point(0, spielfeld.ActualHeight - boarderRectangle), spielfeld.ActualWidth, boarderRectangle);
+            //und der Balken links
+            ZeichneRechteck(new Point(0, 0), boarderRectangle, spielfeld.ActualHeight);
+        }
+        void ZeichneRechteck(Point position, double laenge, double breite)
+        {
+            //einen neuen Balken erzeugen
+            Rectangle balken = new Rectangle();
+            Canvas.SetLeft(balken, position.X);
+            Canvas.SetTop(balken, position.Y);
+            balken.Width = laenge;
+            balken.Height = breite;
+            SolidColorBrush fuellung = new SolidColorBrush(Colors.Red);
+            balken.Fill = fuellung;
+            //der Balken hat den Namen grenze
+            balken.Name = "Grenze";
+            //und hinzufügen
+            spielfeld.Children.Add(balken);
+        }
+        private void Timer_Spielzeit(object sender, EventArgs e)
+        {
+            //die Zeit erhöhen und neu anzeigen
+            remainingTime = remainingTime - 1;
+            zeitAnzeige.Content = remainingTime;
         }
     }
 }

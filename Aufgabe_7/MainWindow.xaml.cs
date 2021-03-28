8214ece5c337c986ec35e878ae9ab2155aff327c
Dirk Mueller <dirkmuller8@outlook.de>
2021-03-28 06:08:50 +0200

    New approach with one button moving only

--- Aufgabe_7/MainWindow.xaml.cs
+++ Aufgabe_7/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Threading;
+using System.Timers;
 
 namespace Aufgabe_7
 {
@@ -25,6 +26,7 @@ namespace Aufgabe_7
         int remainingTime;
         int points;
         DispatcherTimer timerGameTime;
+        DispatcherTimer timerHideTime;
         Random random = new Random();
         Button btn = new Button();
 
@@ -37,11 +39,13 @@ namespace Aufgabe_7
             points = 0;
 
             timerGameTime = new DispatcherTimer();
-            //das Intervall setzen
             timerGameTime.Interval = TimeSpan.FromMilliseconds(1000);
-            //die Methode für das Ereignis zuweisen
-            timerGameTime.Tick += new EventHandler(Timer_Spielzeit);
+            timerGameTime.Tick += new EventHandler(Timer_Game_Duration);
+
+            timerHideTime = new DispatcherTimer();
+            timerHideTime.Interval = TimeSpan.FromMilliseconds(900);
             timerGameTime.Start();
+            timerHideTime.Start();
         }
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
@@ -52,20 +56,15 @@ namespace Aufgabe_7
         }
         void DrawPlayground()
         {
-            //der Balken oben
             DrawRectangle(new Point(0, 0), spielfeld.ActualWidth, boarderRectangle);
-            //der Balken rechts
             DrawRectangle(new Point(spielfeld.ActualWidth - boarderRectangle, 0), boarderRectangle, spielfeld.ActualHeight);
-            //der Balken unten
             DrawRectangle(new Point(0, spielfeld.ActualHeight - boarderRectangle), spielfeld.ActualWidth, boarderRectangle);
-            //und der Balken links
             DrawRectangle(new Point(0, 0), boarderRectangle, spielfeld.ActualHeight);
         }
         void DrawRectangle(Point position, double width, double height)
         {
             Rectangle rectangle = new Rectangle();
 
-            ////einen neuen Balken erzeugen
             Canvas.SetLeft(rectangle, position.X);
             Canvas.SetTop(rectangle, position.Y);
             rectangle.Width = width;
@@ -75,26 +74,50 @@ namespace Aufgabe_7
             rectangle.Name = "Boarder";
             spielfeld.Children.Add(rectangle);
         }
-        private void Timer_Spielzeit(object sender, EventArgs e)
+        //private static Timer timer;
+        private void Timer_Game_Duration(object sender, EventArgs e)
         {
+            //Button btn = new Button();
+            //btn.Height = 40;
+            //btn.Width = 70;
+            //btn.Content = "Click me";
+            //btn.Background = new SolidColorBrush(Colors.Orange);
+            //btn.Foreground = new SolidColorBrush(Colors.Black);
 
-            Button btn = new Button();
-            btn.Height = 40;
-            btn.Width = 70;
-            btn.Content = "Click me";
-            btn.Background = new SolidColorBrush(Colors.Orange);
-            btn.Foreground = new SolidColorBrush(Colors.Black);
-
-            int groesse = 35;
+            //int groesse = 35;
+            int maxLeft = Convert.ToInt32(spielfeld.ActualWidth - button1.Width);
+            int maxTop = Convert.ToInt32(spielfeld.ActualHeight - button1.Height);
             remainingTime = remainingTime - 1;
             timeDisplay.Content = remainingTime;
-            int maxX = (int)spielfeld.ActualWidth - groesse;
-            int maxY = (int)spielfeld.ActualHeight - groesse;
+            button1.Margin = new Thickness(random.Next(maxLeft), random.Next(maxTop), 0, 0);
 
-            Canvas.SetLeft(btn, random.Next(boarderRectangle, maxX));
-            Canvas.SetTop(btn, random.Next(boarderRectangle, maxY));
+            //ProgressBar
+            //int maxX = (int)spielfeld.ActualWidth - groesse;
+            //int maxY = (int)spielfeld.ActualHeight - groesse;
 
-            spielfeld.Children.Add(btn);
+            //Canvas.SetLeft(btn, random.Next(boarderRectangle, maxX));
+            //Canvas.SetTop(btn, random.Next(boarderRectangle, maxY));
+
+            //spielfeld.Children.Add(btn);
+            //timer = new System.Timers.Timer();
+            //timer.Interval = 500;
+            //timer.Start();
+            //spielfeld.Children.Remove(btn);
+        }
+        //private void button1_Click(object sender, MouseEventArgs e)
+        //{
+        //    int maxLeft = Convert.ToInt32(spielfeld.ActualWidth - button1.Width);
+        //    int maxTop = Convert.ToInt32(spielfeld.ActualHeight - button1.Height);
+        //    Random rand = new Random();
+        //    button1.Margin = new Thickness(rand.Next(maxLeft), rand.Next(maxTop), 0, 0);
+        //}
+
+        private void button1_Click_1(object sender, RoutedEventArgs e)
+        {
+            int maxLeft = Convert.ToInt32(spielfeld.ActualWidth - button1.Width);
+            int maxTop = Convert.ToInt32(spielfeld.ActualHeight - button1.Height);
+            Random rand = new Random();
+            button1.Margin = new Thickness(rand.Next(maxLeft), rand.Next(maxTop), 0, 0);
         }
         //private void Timer_Spielzeit(object sender, EventArgs e)
         //{
